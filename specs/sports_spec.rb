ecc62103c24dfb92494020894c1428f5ed71a384
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-09-16 15:41:16 +0100

    Parts A and B complete

--- specs/sports_spec.rb
+++ specs/sports_spec.rb
@@ -25,4 +25,32 @@ class TestTeam < MiniTest::Test
     assert_equal("Big Joe", team.coach)
   end
 
+  def test_add_player
+    team = Team.new("Rovers", ["Roy", "Joy", "Troy"], "Gaffer")
+    team.add_player("Zoe")
+    assert_equal(["Roy", "Joy", "Troy", "Zoe"], team.players)
+  end
+
+  def test_find_player__player_found
+    team = Team.new("Rovers", ["Roy", "Joy", "Troy"], "Gaffer")
+    assert_equal(true, team.find_player("Roy"))
+  end
+
+  def test_find_player__player_not_found
+    team = Team.new("Rovers", ["Roy", "Joy", "Troy"], "Gaffer")
+    assert_equal(false, team.find_player("Bob"))
+  end
+
+  def test_update_points__win
+    team = Team.new("Rovers", ["Roy", "Joy", "Troy"], "Gaffer")
+    team.update_points("win")
+    assert_equal(3, team.points)
+  end
+
+  def test_update_points__loss
+    team = Team.new("Rovers", ["Roy", "Joy", "Troy"], "Gaffer")
+    team.update_points("loss")
+    assert_equal(0, team.points)
+  end
+
 end
--- sports.rb
+++ sports.rb
@@ -1,11 +1,12 @@
 class Team
 
-  attr_accessor(:team_name, :players, :coach)
+  attr_accessor(:team_name, :players, :coach, :points)
 
   def initialize(input_team_name, input_players, input_coach)
     @team_name = input_team_name
     @players = input_players
     @coach = input_coach
+    @points = 0
   end
 
   # def get_team_name()
@@ -23,5 +24,20 @@ class Team
   # def set_coach(new_coach)
   #   @coach = new_coach
   # end
+
+  def add_player(new_player)
+    @players << new_player
+  end
+
+  def find_player(name)
+    for player in @players
+      return true if player == name
+    end
+    return false
+  end
+
+  def update_points(result)
+    @points += 3 if result == "win"
+  end
 
 end
